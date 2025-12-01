ae087dee30130d860ef15708a43c25df866959a4
Mehrin Afroz Lopa <113461725+M-A-Lopa@users.noreply.github.com>
2025-12-01 22:13:36 +0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx';
+import { Routes, Route } from 'react-router-dom';//Lopa
+import Home from './pages/Home.jsx';//Lopa
+//everything under second <div>
 
 const App = () => {
   return (
