469829d0e8fbc62798f13d237870f39d8725224d
Mehrin Afroz Lopa <113461725+M-A-Lopa@users.noreply.github.com>
2025-12-01 22:11:31 +0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
 
 const App = () => {
   return (
-    <div></div>
+    <div>
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
