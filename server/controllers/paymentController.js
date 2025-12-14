ad8699fa6e3f6bbbbdab25585259817b33f138a9
Mehrin Afroz Lopa <113461725+M-A-Lopa@users.noreply.github.com>
2025-12-15 02:01:05 +0600

    Update paymentController.js

---
+++ server/controllers/paymentController.js
@@ -0,0 +1,19 @@
+import stripe from "../configs/stripe.js";
+
+export const createPaymentIntent = async (req, res) => {
+  try {
+    const { amount } = req.body;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount * 100, // cents
+      currency: "usd",
+      automatic_payment_methods: { enabled: true },
+    });
+
+    res.status(200).json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
