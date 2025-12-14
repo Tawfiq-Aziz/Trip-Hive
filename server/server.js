74339764ee3ad712fea730afa4094583e5d70dad
Mehrin Afroz Lopa <113461725+M-A-Lopa@users.noreply.github.com>
2025-12-14 22:03:22 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from './controllers/clerkWebhooks.js';
+import paymentRoutes from "./routes/paymentRoutes.js";//added_atripe
 
 
 connectDB();
@@ -20,8 +21,11 @@ app.use("/api/clerk", clerkWebhooks);
 
 app.get('/', (req, res) => res.send('API IS WORKING'));
 
+app.use("/api/payment", paymentRoutes);//added_stripe
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
