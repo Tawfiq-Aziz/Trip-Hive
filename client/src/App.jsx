ac608ea2bb94551e06acaa22b886537483017f81
Mehrin Afroz Lopa <113461725+M-A-Lopa@users.noreply.github.com>
2025-12-29 21:37:09 +0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { createContext, useContext, useState } from "react";//added
 import Navbar from "./components/Navbar.jsx";
 import { useLocation, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
@@ -9,32 +8,34 @@ import Footer from "./components/Footer.jsx";
 import Checkout from "./pages/Checkout";//added
 import MyBookings from "./pages/MyBookings.jsx";
 import RoomDetails from "./pages/RoomDetails.jsx";
-import { AppContextProvider } from "./context/AppContext";//edited
+import {Toaster} from "react-hot-toast";//imported for connecting backend
 
 
 const App = () => {
   const location = useLocation();
   const isOwnerRoute = location.pathname.includes("/owner");
+  const {showHotelReg} = useAppContext();{/*added for connecting backend */}
 
   return (
-    <AppContextProvider>{/* added AppContextProvider */}
-      <div>
-        {!isOwnerRoute && <Navbar />}
-        <div className="min-h-screen">
-          
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/rooms" element={<AllRooms/>} />
-            <Route path="/owner/*" element={<OwnerLayout />} />
-            <Route path="/my-bookings" element={<MyBookings />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path='/rooms/:id' element={<RoomDetails/>}/>
-          </Routes>
-          
-        </div>
-        <Footer />
+    <div>
+      <Toaster /> {/*added for connecting backend */}
+      {!isOwnerRoute && <Navbar />}
+      {showHotelReg && <HotelReg />}{/*added for connecting backend */}
+      <div className="min-h-screen">
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms/>} />
+          <Route path="/owner/*" element={<OwnerLayout />} />
+          <Route path="/my-bookings" element={<MyBookings />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path='/rooms/:id' element={<RoomDetails/>}/>
+        </Routes>
       </div>
-    </AppContextProvider>
+      <Footer />
+    </div>
+    
   );
 };
+
 export default App;
