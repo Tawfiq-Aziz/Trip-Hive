0fd559f6f9e95bac3a9de6de401d00b87aec60f1
Mehrin Afroz Lopa <113461725+M-A-Lopa@users.noreply.github.com>
2025-12-29 19:59:55 +0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,40 @@
 import React from "react";
+import { createContext, useContext, useState } from "react";//added
 import Navbar from "./components/Navbar.jsx";
 import { useLocation, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import AllRooms from "./pages/AllRooms.jsx";
 import OwnerLayout from "./pages/owner/ownerLayout.jsx";
 import Footer from "./components/Footer.jsx";
-import Checkout from "./pages/Checkout";//added_stripe
+import Checkout from "./pages/Checkout";//added
 import MyBookings from "./pages/MyBookings.jsx";
 import RoomDetails from "./pages/RoomDetails.jsx";
+import { AppContextProvider } from "./context/AppContext";//edited
 
-";
 
 const App = () => {
   const location = useLocation();
   const isOwnerRoute = location.pathname.includes("/owner");
 
   return (
-    <div>
-      {!isOwnerRoute && <Navbar />}
-      <div className="min-h-screen">
-        
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/rooms" element={<AllRooms/>} />
-          <Route path="/owner/*" element={<OwnerLayout />} />
-          <Route path="/my-bookings" element={<MyBookings />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path='/rooms/:id' element={<RoomDetails/>}/>
-        </Routes>
+    <AppContextProvider>{/* added AppContextProvider */}
+      <div>
+        {!isOwnerRoute && <Navbar />}
+        <div className="min-h-screen">
+          
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/rooms" element={<AllRooms/>} />
+            <Route path="/owner/*" element={<OwnerLayout />} />
+            <Route path="/my-bookings" element={<MyBookings />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path='/rooms/:id' element={<RoomDetails/>}/>
+          </Routes>
+          
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-    
+    </AppContextProvider>
   );
 };
-
 export default App;
