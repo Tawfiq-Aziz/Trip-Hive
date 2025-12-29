bea0e3693d70b8d8a1788948890febfdeca9d49e
Mehrin Afroz Lopa <113461725+M-A-Lopa@users.noreply.github.com>
2025-12-29 17:27:00 +0600

    Add files via upload

---
+++ client/src/components/RecommendedHotels.jsx
@@ -0,0 +1,35 @@
+import React from 'react' //full edited and created
+import HotelCard from './HotelCard'
+import Title from './Title'
+import { useNavigate } from 'react-router-dom'
+
+const RecommendedHotels = () => {
+  const{rooms, searchedCities} = useAppContext();
+  const [recommended, setRecommended] = useState([]);
+
+  const filterHotels = () =>{
+    const filteredHotels = rooms.slice().filter( room => searchedCities.includes(room.hotel.city));
+    setRecommended(filteredHotels);
+  }
+
+  useEffect(()=>{
+    filterHotels()
+  },[rooms, searchedCities])
+
+  return recommended.length > 0 && (
+    <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50
+    py-20'>
+
+      <Title title= 'Recommended Hotels' subTitle= 'Discover our handpicked selection of exceptional properties around the world, offering unparalleled luxury and unforgattable experiences. ' />
+
+        <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
+            {recommended.slice(0,4).map((room, index) => (
+                <HotelCard room={room} index={index} key={room._id} />
+            ))}
+        </div>
+
+    </div>
+  )
+}
+
+export default RecommendedHotels
